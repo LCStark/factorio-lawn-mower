c3df3bec3ca055c9657f31ae0fc97312fbf70a42
Lucas Stark <166645+LCStark@users.noreply.github.com>
2025-06-12 11:39:32 +0200

    Added missing config update on mod version change

--- control.lua
+++ control.lua
@@ -47,7 +47,13 @@ script.on_event(defines.events.on_player_alt_selected_area, function(event)
 end)
 
 script.on_event(defines.events.on_built_entity, function(event)
-  if event.entity == nil or event.entity.type == "entity-ghost" or event.entity.type == "tile-ghost" or not event.entity.prototype.selectable_in_game then return end
+  if event.entity == nil or
+     event.entity.type == "entity-ghost" or
+     event.entity.type == "tile-ghost" or
+     not event.entity.prototype.selectable_in_game then
+      return
+  end
+  
   local surface = game.surfaces[event.entity.surface_index]
   local area = event.entity.selection_box
   
@@ -55,7 +61,12 @@ script.on_event(defines.events.on_built_entity, function(event)
 end)
 
 script.on_event(defines.events.on_robot_built_entity, function(event)
-  if event.entity.type == "entity-ghost" or event.entity.type == "tile-ghost" or not event.entity.prototype.selectable_in_game then return end
+  if event.entity.type == "entity-ghost" or
+     event.entity.type == "tile-ghost" or
+     not event.entity.prototype.selectable_in_game then
+      return
+  end
+  
   local surface = game.surfaces[event.entity.surface_index]
   local area = event.entity.selection_box
   
@@ -63,7 +74,12 @@ script.on_event(defines.events.on_robot_built_entity, function(event)
 end)
 
 script.on_event(defines.events.script_raised_built, function(event)
-  if event.entity.type == "entity-ghost" or event.entity.type == "tile-ghost" or not event.entity.prototype.selectable_in_game then return end
+  if event.entity.type == "entity-ghost" or
+     event.entity.type == "tile-ghost" or
+     not event.entity.prototype.selectable_in_game then
+      return
+  end
+  
   local surface = game.surfaces[event.entity.surface_index]
   local area = event.entity.selection_box
   
@@ -71,7 +87,12 @@ script.on_event(defines.events.script_raised_built, function(event)
 end)
 
 script.on_event(defines.events.script_raised_revive, function(event)
-  if event.entity.type == "entity-ghost" or event.entity.type == "tile-ghost" or not event.entity.prototype.selectable_in_game then return end
+  if event.entity.type == "entity-ghost" or
+     event.entity.type == "tile-ghost" or
+     not event.entity.prototype.selectable_in_game then
+      return
+  end
+  
   local surface = game.surfaces[event.entity.surface_index]
   local area = event.entity.selection_box
   
@@ -80,15 +101,25 @@ end)
 
 -- SETTINGS & INITIALIZATION
 
-script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
-  if event.setting ~= "lawnmower-building-clear-range" and event.setting ~= "lawnmower-drop-minable-items" then return end
+function cacheSettings()
   storage.settings = {}
   storage.settings.lawnmower_building_clear_range = settings.global["lawnmower-building-clear-range"].value
   storage.settings.lawnmower_drop_minable_items = settings.global["lawnmower-drop-minable-items"].value
+end
+
+script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
+  if event.setting ~= "lawnmower-building-clear-range" and
+     event.setting ~= "lawnmower-drop-minable-items" then
+      return
+  end
+  
+  cacheSettings()
 end)
 
 script.on_init(function()
-  storage.settings = {}
-  storage.settings.lawnmower_building_clear_range = settings.global["lawnmower-building-clear-range"].value
-  storage.settings.lawnmower_drop_minable_items = settings.global["lawnmower-drop-minable-items"].value
+  cacheSettings()
+end)
+
+script.on_configuration_changed(function()
+  cacheSettings()
 end)
